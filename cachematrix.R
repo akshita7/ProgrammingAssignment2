6fcd704dffc3b32ab432cf1859f034e2bb29ca50
akshita <akshiita.dangi@gmail.com>
2014-04-27 21:06:10 +0530

    assignment2 complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R loads two functions, named, makeCacheMatrix(x = matrix()) and 
+## chachSolve(x, ...), in the environment
 
-## Write a short comment describing this function
+## makeCacheMatrix(x) function creates a data structure to contain the given matrix,
+## its inverse, and functions to retrieve the given matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+        ## initialize inverse 'i' as NULL
+        i <- NULL
+        
+        ## set the given matrix and initial inverse value in the data structure  
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        
+        ## gets the given matrix from the data structure
+        get <- function() x
+        
+        ## sets the inverse matrix in the data structure
+        setinverse <- function(inverse) i <<- inverse
+        
+        ## retrieves the inverse matrix
+        getinverse <- function() i
+        
+        
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve(x, ...) function checks if the inverse matrix of the given matrix is present 
+## in the environment and if found then returns it, otherwise it calcultes the inverse matrix,
+## sets it in the data structure for future use and then returns the inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## retrive the value of inverse matrix from the data structure
+        i <- x$getinverse()
+        
+        ## check if the inverse is present and then return it
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        
+        ## if inverse is NULL, then get the given matrix and solve for inverse
+        data <- x$get()
+        i <- solve(data, ...)
+        
+        ## set the calculated inverse in the data structure
+        x$setinverse(i)
+        
+        ## return inverse matrix
+        i
 }
